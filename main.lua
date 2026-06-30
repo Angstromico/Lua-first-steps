4af43f5c0b920a86050ac79b702476d6cd34d8f2
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-06-30 09:29:48 -0400

    feat: testing VSC comments keyboard shortcuts

--- main.lua
+++ main.lua
@@ -7,3 +7,6 @@ print("Hello World")
  will not run code
  written in between.
 ]]
+
+-- This is another
+-- comment that is not a valid Lua comment because it is not preceded by the double hyphen (--).
