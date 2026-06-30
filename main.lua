44bbacdd62c260474a71a87ebeb47fc0b45fa68f
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-06-30 14:30:05 -0400

    lear(print): no tags with dots

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@
 
 print("Hello World")
 print("This is a test", "And this is another test") -- You can also add comments at the end of a line of code. and in prints you can add multiple arguments separated by commas.
+print("This is a test" .. "And this is another test") -- Use dots for no tags spaces
 
 --[[ this is a multi-line comment.
  The compiler
