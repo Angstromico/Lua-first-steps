8e8f88ad3eac7c8f5e202ef3db580f5fab5a266f
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-06-30 15:34:47 -0400

    learn: data types

---
+++ data-types.lua
@@ -0,0 +1,13 @@
+local null = nil -- Null in Lua is represented by the value nil. It is used to indicate the absence of a value or a non-existent variable.
+
+local number = 42 -- Numbers in Lua can be integers or floating-point numbers. They are used for mathematical operations and calculations.
+
+local text = "Hello, World!" -- Strings in Lua are sequences of characters. They are used to represent text data.
+
+local isTrue = true -- Booleans in Lua have two values: true and false. They are used for logical operations.
+
+local isFalse = false -- Booleans in Lua have two values: true and false. They are used for logical operations.
+
+local isFunc = function() -- Functions in Lua are first-class values. They can be assigned to variables, passed as arguments, and returned from other functions.
+    print("This is a function.")
+end
